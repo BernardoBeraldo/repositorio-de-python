32e3c26b8dd5cfda86f216d66d777b071d9169c2
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-07-15 16:48:33 -0300

    ex 008

--- aula-curso-em-video/exercício-py/ex008.py
+++ aula-curso-em-video/exercício-py/ex008.py
@@ -1,1 +1,9 @@
-m = float(input('digite médida'))
+m = float(input('digite médida em metros: '))
+mm = m / 100
+cm = m / 10
+m = m 
+dm = m * 10
+dam = m * 100
+hm = m * 1000
+km = m * 10000
+print('as medidas mm = {} \n cm = {} \n m = {} \n dm = {} \n dam = {} \n hm = {} \n km = {} '.format(mm, cm, m, dm, dam, hm, km))

3036fa718cf541ffe4bee3b3906344bd0e44ec21
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-07-15 11:06:03 -0300

    ex4

--- aula-curso-em-video/exercício-py/ex004.py
+++ aula-curso-em-video/exercício-py/ex004.py
@@ -1,6 +1,6 @@
-v1= int(input('digite um numero racionar: '))
+v1 = int(input('digite um numero racionar: '))
 print(v1)
-v1= float(input('digite um nummero real: '.format(v1,) ))
+v1= float(input('digite um nummero real: {}'.format(v1,) ))
 v2 = input('digide aldo:')
 print('verificação se é um numero:',v2.isnumeric())
 print('verificação se é letra:',v2.isalpha())

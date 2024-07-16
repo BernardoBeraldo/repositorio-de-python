69aa33091c73e18736da8e6291b2f0dc5d2fad0e
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-07-16 16:36:37 -0300

    ex010

--- aula-curso-em-video/exercício-py/ex010.py
+++ aula-curso-em-video/exercício-py/ex010.py
@@ -1,3 +1,5 @@
-n = float(input('digite um numero :'))
-c = n/5.44
-print('o valor  me real  {} em dolar {}'.format(n,c))
+n = float(input('digite um valor em real: '))
+d = n / 5.44
+e = n / 5.91
+print('o valor  em real  {} em dolar {}'.format(n,d))
+print('o valor em real {} em euro {}'.format(n,e))

544e1dbfe67a3efc98a298159d72353de94d6dc1
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-07-19 10:55:15 -0300

    ex018

---
+++ aula-curso-em-video/exercício-py/ex018.py
@@ -0,0 +1,7 @@
+#Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+import math
+num = float(input('digite um numero '))
+con = math.cos(num)
+sen = math.sin(num)
+tan = math.tan(num)
+print('os valores de cos: {:.3f} sin: {:.3f} tan: {:.3f}'.format(con, sen, tan))

dc87c01b90fa4c886b8f032275c27d919bb17ff7
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-07-22 13:19:16 -0300

    ex021

---
+++ aula-curso-em-video/exercício-py/ex022.py
@@ -0,0 +1,4 @@
+#Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre
+f = '''Nessa aula, vamos aprender operações com String no Python. As principais operações que vamos aprender são o Fatiamento de String,
+Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().'''
+print(f)
--- aula-curso-em-video/teste/04.py
+++ aula-curso-em-video/teste/04.py
@@ -4,3 +4,10 @@ import math
 num =  int(input('digite um numero: '))
 raiz = math.sqrt(num)
 print('A rais quadrada de {} é {}'.format(num, raiz))
+
+'''Nessa aula, vamos aprender operações com String no Python.
+As principais operações que vamos aprender são o 
+Fatiamento de String, Análise com 
+len(), count(), find(), transformações
+com replace(), upper(), lower(), capitalize(), 
+title(), strip(), junção com join().'''

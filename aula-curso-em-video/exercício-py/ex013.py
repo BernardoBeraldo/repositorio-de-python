c3e899d39d3170b4f81176252590ce83da97133a
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:42:34 -0300

    ex013

--- aula-curso-em-video/exercício-py/ex013.py
+++ aula-curso-em-video/exercício-py/ex013.py
@@ -1,4 +1,5 @@
-#Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+#Exercício Python 13: 
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 salário = float(input('digite o valor do salário em R$ : '))
 novo = salário + (salário * 15 / 100)
 print('O valor de novo salario é {:.2f}'.format(novo))

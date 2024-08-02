5b0f4674afc296db309039faae5a21c9bc0c27a1
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:45:46 -0300

    ex022

--- aula-curso-em-video/exercício-py/ex022.py
+++ aula-curso-em-video/exercício-py/ex022.py
@@ -1,11 +1,11 @@
-'''
-Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostreExercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
 
- * O nome com todas as letras maiúsculas e minúsculas.
- Quantas letras ao todo (sem considerar espaços).
- Quantas letras tem o primeiro nome.
+#Exercício Python 22:
+#Crie um programa que leia o nome completo de uma pessoa e mostreExercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
 
- '''
+ #O nome com todas as letras maiúsculas e minúsculas.
+ #Quantas letras ao todo (sem considerar espaços).
+ #Quantas letras tem o primeiro nome.
+
 nome = str(input('digite um nome : ')).strip()
 print('seu nome em maiúscula {}'.format(nome.upper()))
 print('seu nome em minúsculas {}'.format(nome.lower()))

c46a91823a1c743cf44a4135d3bab1c5efcd2adc
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:44:46 -0300

    ex020

--- aula-curso-em-video/exercício-py/ex020.py
+++ aula-curso-em-video/exercício-py/ex020.py
@@ -1,5 +1,5 @@
 #Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. 
-# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 import random
 a1 = (input('aluno 1 ; '))
 a2 = (input('aluno 2 ; '))

3a835e15739c9a1a2282cf769d579b3577765918
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:38:08 -0300

    ex007

--- aula-curso-em-video/exercício-py/ex007.py
+++ aula-curso-em-video/exercício-py/ex007.py
@@ -1,4 +1,5 @@
-# Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+# Exercício Python 7: 
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 n1 = float(input( 'Digite a primeira nota|: '))
 n2  = float (input('Digite a segunda nota: '))
 m = (n1+n2)/2

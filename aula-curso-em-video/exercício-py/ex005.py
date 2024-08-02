77ed8fc036f64f896a7f569d54e04f51b4550707
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:37:42 -0300

    ex005

--- aula-curso-em-video/exercício-py/ex005.py
+++ aula-curso-em-video/exercício-py/ex005.py
@@ -1,3 +1,4 @@
-# Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+# Exercício Python 5: 
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
 n = int(input('digite um nomero: '))
 print(n-1, n , n+1)

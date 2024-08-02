cefd3ea13e947a34a2a276dcdf69899c1ba0999f
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:49:39 -0300

    ex024

--- aula-curso-em-video/exercício-py/ex024.py
+++ aula-curso-em-video/exercício-py/ex024.py
@@ -1,2 +1,2 @@
-print('Exercício  Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.')
+#print Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
  

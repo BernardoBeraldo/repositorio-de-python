d62b2a0551b58340ef20e2f2165a15add85e023d
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:36:48 -0300

    ex002

--- aula-curso-em-video/exercício-py/ex002.py
+++ aula-curso-em-video/exercício-py/ex002.py
@@ -1,3 +1,4 @@
-# Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+# Exercício Python 2
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 nome = input('digite seu nome =')
 print('Bem-vindo',nome,)

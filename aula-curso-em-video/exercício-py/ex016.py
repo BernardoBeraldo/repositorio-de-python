4e307c12992a3ee1f12c101cc5b870c52f03579f
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:43:29 -0300

    ex016

--- aula-curso-em-video/exercício-py/ex016.py
+++ aula-curso-em-video/exercício-py/ex016.py
@@ -1,4 +1,5 @@
-#Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+#Exercício Python 16: 
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 from math import trunc
 num = float(input('digite um numero com ponto (ex 4.5 458.6): '))
 num_inteiro = trunc(num)

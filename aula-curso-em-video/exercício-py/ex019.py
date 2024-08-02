62aff7da799fa6858cf17487b88dbb927e11d62b
Berbardo Beraldo <bernardo.beraldo@gmail.com>
2024-08-02 15:50:09 -0300

    ex019

--- aula-curso-em-video/exercício-py/ex019.py
+++ aula-curso-em-video/exercício-py/ex019.py
@@ -1,10 +1,14 @@
 #Exercício Python 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 #Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
 import random
+
 a1 = str(input('digite o nome de um aluno; '))
 a2 = str(input('digite o nome de um aluno; '))
 a3 = str(input('digite o nome de um aluno; '))
 a4 =  str(input('digite o nome de um aluno; '))
+
 lista = [a1, a2, a3, a4]
 escolhido = random.choice(lista)
+
 print('O aluno sortiado foi {}'.format(escolhido))
